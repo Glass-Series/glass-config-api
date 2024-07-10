78cecf2c3da7ce5bd8e3cddf4bda5bfa908c3a6a
calmilamsy <bumbill00@gmail.com>
2024-07-10 20:31:57 +0100

    Update boom, move to kotlin dsl
    This is my project, so uh, fuck you groovy, yeet, bye, be gone, I hope I don't have to run a full project with you ever again

---
+++ build.gradle.kts
@@ -0,0 +1,113 @@
+import java.net.URI
+
+plugins {
+	id("maven-publish")
+	id("fabric-loom") version "1.6.8"
+	id("babric-loom-extension") version "1.6.8"
+}
+
+//noinspection GroovyUnusedAssignment
+java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
+
+//archivesBaseName = project.properties["archives_base_name"]
+version = project.properties["mod_version"] as String
+group = project.properties["maven_group"] as String
+
+loom {
+	customMinecraftManifest.set("https://babric.github.io/manifest-polyfill/${project.properties["minecraft_version"]}.json")
+	intermediaryUrl.set("https://maven.glass-launcher.net/babric/babric/intermediary/%1\$s/intermediary-%1\$s-v2.jar")
+}
+
+repositories {
+	maven("https://maven.glass-launcher.net/snapshots/")
+	maven("https://maven.glass-launcher.net/releases/")
+	maven("https://maven.glass-launcher.net/babric")
+	maven("https://maven.minecraftforge.net/")
+	maven("https://jitpack.io/")
+	mavenCentral()
+	exclusiveContent {
+		forRepository {
+			maven("https://api.modrinth.com/maven")
+		}
+		filter {
+			includeGroup("maven.modrinth")
+		}
+	}
+}
+
+dependencies {
+	minecraft("com.mojang:minecraft:${project.properties["minecraft_version"]}")
+	mappings("net.glasslauncher:biny:${project.properties["yarn_mappings"]}:v2")
+	modImplementation("babric:fabric-loader:${project.properties["loader_version"]}")
+
+	implementation("org.apache.logging.log4j:log4j-core:2.17.2")
+
+	implementation("org.slf4j:slf4j-api:1.8.0-beta4")
+	implementation("org.apache.logging.log4j:log4j-slf4j18-impl:2.17.1")
+
+	// convenience stuff
+	// adds some useful annotations for data classes. does not add any dependencies
+	compileOnly("org.projectlombok:lombok:1.18.24")
+	annotationProcessor("org.projectlombok:lombok:1.18.24")
+
+	// adds some useful annotations for miscellaneous uses. does not add any dependencies, though people without the lib will be missing some useful context hints.
+	implementation("org.jetbrains:annotations:23.0.0")
+
+	modImplementation("net.modificationstation:StationAPI:${project.properties["stapi_version"]}")
+
+	modImplementation("net.glasslauncher.mods:ModMenu:${project.properties["modmenu_version"]}") {
+	}
+
+	implementation(include("blue.endless:jankson:1.2.1") as Dependency)
+	include("com.google.guava:guava:31.1-jre")
+}
+
+tasks.withType<ProcessResources> {
+	inputs.property("version", project.properties["version"])
+
+	filesMatching("fabric.mod.json") {
+		expand(mapOf("version" to project.properties["version"]))
+	}
+}
+
+// ensure that the encoding is set to UTF-8, no matter what the system default is
+// this fixes some edge cases with special characters not displaying correctly
+// see http://yodaconditions.net/blog/fix-for-java-file-encoding-problems-with-gradle.html
+tasks.withType<JavaCompile> {
+	options.encoding = "UTF-8"
+}
+
+java {
+	// Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
+	// if it is present.
+	// If you remove this line, sources will not be generated.
+	withSourcesJar()
+}
+
+tasks.withType<Jar> {
+	from("LICENSE") {
+		rename { "${it}_${project.properties["archivesBaseName"]}" }
+	}
+}
+
+publishing {
+	repositories {
+		mavenLocal()
+		if (project.hasProperty("glass_maven_username")) {
+			maven {
+				url = URI("https://maven.glass-launcher.net/releases")
+				credentials {
+					username = "${project.properties["glass_maven_username"]}"
+					password = "${project.properties["glass_maven_password"]}"
+				}
+			}
+		}
+	}
+
+	publications {
+		register("mavenJava", MavenPublication::class) {
+			from(components["java"])
+		}
+	}
+}
