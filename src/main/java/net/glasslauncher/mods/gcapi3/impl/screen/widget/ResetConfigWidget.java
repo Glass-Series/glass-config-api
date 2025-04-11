af4fcea9ce9c71c6cfe46ec1e8fd9e99f89ecc4c
calmilamsy <bumbill00@gmail.com>
2025-04-11 18:44:56 +0100

    Makre reset config less garbage

--- src/main/java/net/glasslauncher/mods/gcapi3/impl/screen/widget/ResetConfigWidget.java
+++ src/main/java/net/glasslauncher/mods/gcapi3/impl/screen/widget/ResetConfigWidget.java
@@ -1,28 +1,45 @@
 package net.glasslauncher.mods.gcapi3.impl.screen.widget;
 
+import net.glasslauncher.mods.gcapi3.impl.GCCore;
+import net.glasslauncher.mods.gcapi3.impl.object.ConfigCategoryHandler;
 import net.glasslauncher.mods.gcapi3.impl.object.ConfigEntryHandler;
+import net.glasslauncher.mods.gcapi3.mixin.client.MinecraftAccessor;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class ResetConfigWidget extends IconWidget {
     private final ConfigEntryHandler<?> configEntry;
+    private final ConfigCategoryHandler configCategory;
 
     public ResetConfigWidget(ConfigEntryHandler<?> configEntry) {
+        this(configEntry, null);
+    }
+
+    public ResetConfigWidget(ConfigCategoryHandler configCategory) {
+        this(null, configCategory);
+    }
+
+    protected ResetConfigWidget(ConfigEntryHandler<?> configEntry, ConfigCategoryHandler configCategory) {
         super(0, 0, 0, 0, "/assets/gcapi3/reset.png");
         this.configEntry = configEntry;
+        this.configCategory = configCategory;
     }
 
     @Override
     public void mouseClicked(int mouseX, int mouseY, int button) {
         if (mouseX >= this.x && mouseY >= this.y && mouseX < this.x + this.width && mouseY < this.y + this.height) {
             try {
-                if(!configEntry.multiplayerLoaded) {
-                    configEntry.reset(configEntry.defaultValue);
+                if (configEntry != null) {
+                    configEntry.resetMultiplayerSafeRecursive();
                 }
+                else if (configCategory != null) {
+                    configCategory.resetMultiplayerSafeRecursive();
+                }
+                MinecraftAccessor.getInstance().soundManager.method_2009("random.click", 1.0F, 1.0F);
             } catch (IllegalAccessException e) {
-                e.printStackTrace();
+                //noinspection deprecation
+                GCCore.logError(e);
             }
         }
     }
@@ -34,15 +51,21 @@ public class ResetConfigWidget extends IconWidget {
 
     @Override
     public List<String> getTooltip() {
-        if(configEntry.multiplayerLoaded) {
+        if (configEntry != null && configEntry.multiplayerLoaded) {
             return new ArrayList<>();
         }
-        return Collections.singletonList("Reset this config to default.");
+        if (configCategory != null) {
+            return List.of("Reset all inside to default.");
+        }
+        return List.of("Reset this config to default.");
     }
 
     @Override
     public void draw(int mouseX, int mouseY) {
-        if(!configEntry.multiplayerLoaded) {
+        if (configEntry != null && !configEntry.multiplayerLoaded) {
+            super.draw(mouseX, mouseY);
+        }
+        else if (configCategory != null) {
             super.draw(mouseX, mouseY);
         }
     }
